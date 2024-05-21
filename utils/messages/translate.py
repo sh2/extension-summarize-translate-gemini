2ab0ebf2d0d1a97da61044b5b7d0682dd7f310ad
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2024-05-21 17:25:25 +0900

    chore: Add .gitignore and requirements.txt for messages module

---
+++ utils/messages/translate.py
@@ -0,0 +1,61 @@
+import google.generativeai
+import json
+import os
+
+
+def main():
+    languageList = {
+        "de": "German",
+        "es": "Spanish",
+        "fr": "French",
+        "it": "Italian",
+        "pt_BR": "Brazilian Portuguese",
+        "ru": "Russian",
+        "zh_CN": "Simplified Chinese",
+        "zh_TW": "Traditional Chinese",
+        "ko": "Korean"
+    }
+
+    google.generativeai.configure(
+        api_key=os.environ.get("GEMINI_API_KEY", "")
+    )
+
+    with open("../../extension/_locales/en/messages.json", 'r') as f:
+        messages = f.read()
+
+    for languageCode, languageName in languageList.items():
+        print(languageCode, languageName)
+
+        system_instruction = f"Translate the following JSON content to {languageName}" \
+            " in a formal tone. The word \"Gemini\" should be left in English."
+
+        model = google.generativeai.GenerativeModel(
+            model_name="gemini-1.5-flash-latest",
+            system_instruction=system_instruction,
+            generation_config={
+                "response_mime_type": "application/json",
+                "temperature": 0.0
+            }
+        )
+
+        try:
+            response = model.generate_content(messages)
+
+            json_obj = json.loads(
+                response.candidates[0].content.parts[0].text
+            )
+
+            json_text = json.dumps(
+                json_obj, indent=4, ensure_ascii=False
+            )
+
+            os.makedirs(f"output/{languageCode}", exist_ok=True)
+
+            with open(f"output/{languageCode}/messages.json", 'w') as f:
+                f.write(json_text)
+        except Exception as e:
+            print("Failed to generate content:", e)
+
+
+if __name__ == '__main__':
+    main()
