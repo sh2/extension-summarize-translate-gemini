6818cf0054b5c844aae4de8d73db4a1d890d8691
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2025-06-15 20:40:00 +0900

    Add protobuf files for video metadata processing

---
+++ utils/protobuf/translate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+npx pbjs --target static --out video-metadata.js video-metadata.proto
+
+# You need to edit the following 2 lines of the output file.
+#
+# -var $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
+# +var $Reader = protobuf.Reader, $Writer = protobuf.Writer, $util = protobuf.util;
+#
+# -var $root = $protobuf.roots["default"] || ($protobuf.roots["default"] = {});
+# +var $root = protobuf.roots["default"] || (protobuf.roots["default"] = {});
